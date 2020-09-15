fbd06c137c8a72ad5a8ee23908401fd23120ae2d
Arnaud Tisset <a.tisset@reply.com>
2020-09-15 16:14:31 -0700

    added a fedora32-server and opejdk8 installation with a GraalVM installation

--- custom-script.sh
+++ custom-script.sh
@@ -7,3 +7,37 @@ set -eux
 #
 # For example:
 # dnf install -y curl wget git tmux firefox xvfb
+
+JDK_VERSION=${JDK_VERSION:-java-1.8.0-openjdk}
+
+if [ "$INSTALL_OPENJDK" = "true" ] || [ "$INSTALL_OPENJDK" = "1" ]; then
+	# Add vagrant user (if it doesn't already exist)
+	sudo dnf install -y "${JDK_VERSION}.x86_64" "${JDK_VERSION}-devel.x86_64"
+fi
+
+GraalVM_RELEASE_VERSION=${GraalVM_RELEASE_VERSION:-vm-20.2.0/graalvm-ce-java8-linux-amd64-20.2.0}
+GraalVM_RELEASE_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/${GraalVM_RELEASE_VERSION}.tar.gz"
+
+wget -q ${GraalVM_RELEASE_URL}
+wget ${GraalVM_RELEASE_URL} -qO - | sudo tar -xzvf - -C /opt
+
+#Prepend the GraalVM bin directory to the PATH environment variable: $ export PATH=<path to GraalVM>/bin:$PATH 
+#To verify whether you are using GraalVM, run: $ which java
+echo 'export PATH=/opt/graalvm-ce-java8-20.2.0/bin:$PATH' >> ~/.bashrc
+
+#Set the JAVA_HOME environment variable to resolve to the GraalVM installation directory:
+echo 'export JAVA_HOME=/opt/graalvm-ce-java8-20.2.0' >> ~/.bashrc
+
+#source ~/.bashrc
+#java -version
+
+#sudo /opt/graalvm-ce-java8-20.2.0/bin/gu install native-image
+#sudo /opt/graalvm-ce-java8-20.2.0/bin/gu install llvm-toolchain
+
+
+# To install the LLVM toolchain component, run:
+#gu install llvm-toolchain
+
+
+# To install GraalVM Native Image, run:
+# gu install native-image
--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -5,10 +5,9 @@ SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     echo "==> Installing VirtualBox guest additions"
-    # Need to set the KERN_DIR in Fedora 26 and 27 or the VBox additions will not install
-    python -mplatform | grep -qi "fedora-2[67]" && echo "Fedora 26 or 27 detected" && \
-        KERN_DIR=/usr/src/kernels/"$(uname -r)" && export KERN_DIR
-    python -mplatform | grep -qi "fedora-28" && echo "Fedora 28 detected" && \
+    # Need to set the KERN_DIR in Fedora 26/27/28/29/30/31/32 or the VBox additions will not install
+    # Version specific is not needed anymore ?
+    python -mplatform | grep -qi "fedora-32" && echo "Fedora 32 detected" && \
         KERN_DIR=/lib/modules/"$(uname -r)"/build && export KERN_DIR
     if [ "x$KERN_DIR" != "x" ]; then
         # Some of these packages should already have been installed in the kickstart
@@ -18,7 +17,6 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
         sh /mnt/VBoxLinuxAdditions.run --nox11
         umount /mnt
     else
-        echo "Couldn't identify Fedora between 26/27 and 28: couldn't set KERN_DIR."
         echo "Canceling installation of VirtualBox guest additions"
     fi
     rm -rf $SSH_USER_HOME/VBoxGuestAdditions_$VBOX_VERSION.iso
